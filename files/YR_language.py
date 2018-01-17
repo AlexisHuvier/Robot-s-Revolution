87041f584eb5de82681d7b040a6276d561346949
LavaPower <lavapower84@gmail.com>
2018-01-17 18:29:00 +0100

    Add "loopif()" instruction

--- files/YR_language.py
+++ files/YR_language.py
@@ -1,7 +1,7 @@
 import pygame, sys
 from tkinter.messagebox import *
 
-instructions = ["walk", "left", "right", "setSprite","loop", "sayConsole","if_", "getPosX", "getPosY"]
+instructions = ["walk", "left", "right", "setSprite", "loopif", "loop", "sayConsole","if_", "getPosX", "getPosY"]
 
 class Script():
     def __init__(self, robot, fichier):
@@ -120,6 +120,25 @@ class Script():
                 showerror("ERREUR","Erreur sur l'instruction à la loop de la ligne n°"+str(self.avancement+1))
                 pygame.quit()
             
+    def loopif(self, instruction = "walk()", condition = "True"):
+        if condition.split("(")[0] in instructions:
+            if eval("self."+condition):
+                if instruction.split("(")[0] in instructions:
+                    eval("self."+instruction)
+                    self.avancement -=1
+                    self.temp_boucle_a_faire -= 1
+                else:
+                    showerror("ERREUR","Erreur sur l'instruction à la loop de la ligne n°"+str(self.avancement+1))
+                    pygame.quit()
+        else:
+            if eval(condition):
+                if instruction.split("(")[0] in instructions:
+                    eval("self."+instruction)
+                    self.avancement -=1
+                    self.temp_boucle_a_faire -= 1
+                else:
+                    showerror("ERREUR","Erreur sur l'instruction à la loop de la ligne n°"+str(self.avancement+1))
+                    pygame.quit()
     def if_(self, instruction, condition = "True"):
         if condition.split("(")[0] in instructions:
             if eval("self."+condition):
