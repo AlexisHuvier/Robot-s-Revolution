77bb8803f5ed8aef8922344f2173b3f4dacd4172
LavaPower <lavapower84@gmail.com>
2018-01-22 19:18:51 +0100

    Make a class for the map of the game

--- files/RR_Game.py
+++ files/RR_Game.py
@@ -4,7 +4,7 @@ from tkinter.messagebox import *
 
 
 class Game():
-	def __init__(self, fichier, mode, level = 1):
+	def __init__(self, script, mode, level = 1):
 		pygame.init()
         
 		self.result = 0
@@ -18,22 +18,12 @@ class Game():
 		self.done = True
 		if self.mode == "Parcours":
 			self.level = level
-			self.player = Player(fichier, self.level)
 			self.mode = mode
-			self.player_list = pygame.sprite.Group()
-			self.player_list.add(self.player)
-			self.finish_list = pygame.sprite.Group()
-			self.rock_list = pygame.sprite.Group()
 			try:
 				with open("levels/"+str(self.level)+".rev", 'r') as fichier:
 					lignes = fichier.read().split("\n")
-					self.player.rect.x = int(lignes[0].split(",")[0])
-					self.player.rect.y = int(lignes[0].split(",")[1])
-					self.finish = Finish([int(lignes[len(lignes)-1].split(",")[0]),int(lignes[len(lignes)-1].split(",")[1])])
-					self.finish_list.add(self.finish)
-					for i in range(1, len(lignes)-1):
-						self.rock = Rock([int(lignes[i].split(",")[0]),int(lignes[i].split(",")[1])])
-						self.rock_list.add(self.rock)
+					self.map = Map(lignes, script, level)
+					
 			except IOError:
 				showerror("ERREUR","Le fichier du level "+str(self.level)+" est inaccessible")
 				pygame.quit()
@@ -51,15 +41,15 @@ class Game():
 					self.done = False
 					self.result = 0
 
-			self.result = self.player.update(self.rock_list, self.finish_list)
+			self.result = self.map.player.update()
             
 			if self.result == 1:
 				self.screen.fill((0,0,0))
 				self.clock.tick(60)
 				self.screen.blit(pygame.image.load("files/background.png"), [0,0])
-				self.player_list.draw(self.screen)
-				self.rock_list.draw(self.screen)
-				self.finish_list.draw(self.screen)
+				self.map.player_list.draw(self.screen)
+				self.map.rock_list.draw(self.screen)
+				self.map.finish_list.draw(self.screen)
 				pygame.display.update()
 			else:
 				self.done = False
--- files/RR_class.py
+++ files/RR_class.py
@@ -2,54 +2,71 @@ import pygame
 from RR_language import *
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, fichier, level):
-		super(Player, self).__init__()
-		
-		self.strImage = "files/FlammyD.png"
-		self.image = pygame.image.load(self.strImage)
-		self.rect = self.image.get_rect()
-		self.rect.x = 10
-		self.rect.y = 10
-		self.script = Script(self, fichier)
-		self.direction = 0
-		self.attack = False
-		self.posX = 1
-		self.posY = 1
-		self.timer = 10
-		self.level = level
-	
-	def update(self, collidable = pygame.sprite.Group(), collidable2 = pygame.sprite.Group()):
-		self.timer -= 1
-		result = 1
-		if self.timer == 0:
-			result = self.script.launch()
-			self.timer = 10
-		collision_list = pygame.sprite.spritecollide(self, collidable, False, None)
-		for collided_object in collision_list:
 			pygame.quit()
 			result = 0
 			showinfo("Perdu", "Votre robot a touché un caillou !")
-		collision_list_2 = pygame.sprite.spritecollide(self, collidable2, False, None)
-		for collided_object in collision_list_2:
-			pygame.quit()
-			result = self.level + 1
-			showinfo("Gagné", "Votre robot a atteint le point final !")
-		return result
+    def __init__(self, fichier, level, map):
+        super(Player, self).__init__()
+        
+        self.strImage = "files/FlammyD.png"
+        self.image = pygame.image.load(self.strImage)
+        self.rect = self.image.get_rect()
+        self.rect.x = 10
+        self.rect.y = 10
+        self.script = Script(self, fichier)
+        self.direction = 0
+        self.attack = False
+        self.posX = 1
+        self.posY = 1
+        self.timer = 10
+        self.level = level
+        self.map = map
+    
+    def update(self, collidable = pygame.sprite.Group(), collidable2 = pygame.sprite.Group()):
+        self.timer -= 1
+        result = 1
+        if self.timer == 0:
+            result = self.script.launch()
+            self.timer = 10
+        collision_list = pygame.sprite.spritecollide(self, self.map.rock_list, False, None)
+        for collided_object in collision_list:
+        collision_list_2 = pygame.sprite.spritecollide(self, self.map.finish_list, False, None)
+        for collided_object in collision_list_2:
+            pygame.quit()
+            result = self.level + 1
+            showinfo("Gagné", "Votre robot a atteint le point final !")
+        return result
 
 class Rock(pygame.sprite.Sprite):
-	def __init__(self, pos = [10,10]):
-		super(Rock, self).__init__()
-		
-		self.image = pygame.image.load("files/Caillou.png")
-		self.rect = self.image.get_rect()
-		self.rect.x = pos[0]
-		self.rect.y = pos[1]
+    def __init__(self, pos = [10,10]):
+        super(Rock, self).__init__()
+        
+        self.image = pygame.image.load("files/Caillou.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
 class Finish(pygame.sprite.Sprite):
-	def __init__(self, pos = [10,10]):
-		super(Finish, self).__init__()
-		
-		self.image = pygame.image.load("files/finish.png")
-		self.rect = self.image.get_rect()
-		self.rect.x = pos[0]
-		self.rect.y = pos[1]
+    def __init__(self, pos = [10,10]):
+        super(Finish, self).__init__()
+        
+        self.image = pygame.image.load("files/finish.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+
+class Map():
+    def __init__(self, objets, fichier, level):
+        self.player = Player(fichier, level, self)
+        self.player_list = pygame.sprite.Group()
+        self.player_list.add(self.player)
+        self.finish_list = pygame.sprite.Group()
+        self.rock_list = pygame.sprite.Group()
+        self.player.rect.x = int(objets[0].split(",")[0])
+        self.player.rect.y = int(objets[0].split(",")[1])
+        self.finish = Finish([int(objets[len(objets)-1].split(",")[0]),int(objets[len(objets)-1].split(",")[1])])
+        self.finish_list.add(self.finish)
+        for i in range(1, len(objets)-1):
+            self.rock = Rock([int(objets[i].split(",")[0]),int(objets[i].split(",")[1])])
+            self.rock_list.add(self.rock)
+        
